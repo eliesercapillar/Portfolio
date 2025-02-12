05acabf71c2b5a4738a06d1d8660dff703bcfdb9
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-12 00:46:50 -0600

    Revert "Added shadcn-vue package & dependencies"
    This reverts commit 3edade8ca55ac4092b6e99d831dd79279d0d92fa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind, autoprefixer()],
-    },
-  },
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
 })
