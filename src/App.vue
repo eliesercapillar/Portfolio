7a0c79f8a40e0f096b44a1a784bd55be9b7e654c
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-13 17:41:48 -0600

    Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent } from 'vue';
+
+const NavBar = defineAsyncComponent(() => import(`./components/NavBar.vue`))
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './css/style.css'
+import './css/tailwind.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
