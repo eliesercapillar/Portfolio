3fd29f43c744d68c362648787c9306d78ad94a16
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-17 17:31:57 -0600

    Added logic for light/dark mode technology icons

--- src/components/Technologies.vue
+++ src/components/Technologies.vue
@@ -6,7 +6,7 @@
                 <Card v-for="t in Tech.data" :key="t.name" 
                     class="border text-card-foreground shadow overflow-hidden rounded-md">
                     <CardContent class="flex aspect-square select-none flex-col items-center justify-center p-2 gap-2">
-                        <Icon :icon="t.icon" class="size-12 max-w-full max-h-full"></Icon>
+                        <Icon :icon="getIcon(t).value" class="size-12 max-w-full max-h-full"></Icon>
                         <CardTitle class="text-sm text-center ">{{ t.name }}</CardTitle>
                     </CardContent>
                 </Card>
@@ -17,10 +17,25 @@
 
 <script setup lang="ts">
 import Tech from "@/data/Technologies.json"
-import {
-  Card,
-  CardContent,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card, CardContent, CardTitle } from '@/components/ui/card'
 import { Icon } from '@iconify/vue';
+import { loadIcon, loadIcons } from '@iconify/vue';
+import { useColorMode } from '@vueuse/core'
+import { computed } from 'vue';
+import { onMounted } from "vue";
+
+const mode = useColorMode()
+
+const getIcon = (t: any) => computed(() => {
+    if (!t.hasMultipleIcons) return t.icon;
+    return mode.value === 'light' ? t.icon_light : t.icon_dark;
+});
+
+onMounted(() => {
+    Tech.data.forEach((t: any) => {
+        if (t.hasMultipleIcons) loadIcons([t.icon_light, t.icon_dark])
+        else loadIcon(t.icon)
+    })
+})
+
 </script>
