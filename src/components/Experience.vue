070a9affca43e1f39bef1beae46ec51ab1e15907
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-22 03:28:09 -0600

    Added more projects

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,15 +2,15 @@
     <div class="mx-auto flex max-w-3xl antialiased w-full">
         <section class="mt-8 flex flex-col w-full">
             <h2 class="text-3xl mb-4 text-foreground font-supreme font-medium">Experience</h2>
-            <Tabs default-value="experience" class="w-full rounded-md">
+            <Tabs default-value="work" class="w-full rounded-md">
                 <TabsList class="w-full flex shadow-md">
-                    <TabsTrigger value="experience" class="flex-1 font-supreme font-normal" >Experience</TabsTrigger>
-                    <TabsTrigger value="education" class="flex-1 font-supreme font-normal">Education</TabsTrigger>
+                    <TabsTrigger value="work" class="flex-1 font-supreme font-normal" >Professional</TabsTrigger>
+                    <TabsTrigger value="school" class="flex-1 font-supreme font-normal">Education</TabsTrigger>
                 </TabsList>
-                <TabsContent value="experience">
+                <TabsContent value="work">
                     <Timeline :entries="Career.data" key="career"/>
                 </TabsContent>
-                <TabsContent value="education">
+                <TabsContent value="school">
                     <Timeline :entries="Education.data" key="education"/>
                 </TabsContent>
             </Tabs>
