214ca9ff556432e1ca86c6895ebfb0f76ab29eb7
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-23 20:05:05 -0600

    Updated 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -9,7 +9,8 @@
             <span class="text-3xl md:text-7xl opacity-60"> | </span>
             <p class="text-3xl md:text-7xl opacity-60">¯\_(ツ)_/¯</p>
           </div>
-          <div class="flex items-center justify-center">
+          <div class="flex flex-col items-center gap-4">
+            <p class="text-lg md:text-3xl opacity-60 text-center">Can't seem to find this page...</p>
             <a href="#" class="flex items-center gap-2 text-muted-foreground hover:text-foreground">
                 <Icon icon="lucide:arrow-left" class="size-7"/>
                 <p class="font-light text-lg md:text-3xl">Back to Home</p>
@@ -19,12 +20,6 @@
         <Footer/>
       </div>
     </div>
-
-    <!-- <div class="flex h-screen flex-col items-center justify-center text-center">
-      <h1 class="text-4xl font-bold">404 - Page Not Found</h1>
-      <p class="mt-4 text-lg">The page you're looking for doesn't exist.</p>
-      <router-link to="/" class="mt-6 text-blue-500 hover:underline">Go Home</router-link>
-    </div> -->
 </template>
 
 <script setup lang="ts">
