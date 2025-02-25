bdf2f10b575ccb80cb2e59b5e472581fe251c83d
Elieser Capillar <elieser.capillar@hotmail.com>
2025-02-25 13:58:24 -0600

    Removed deployment script and subpath

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio' : '/', // Use '/' for dev, '/repo-name/' for prod
 })
